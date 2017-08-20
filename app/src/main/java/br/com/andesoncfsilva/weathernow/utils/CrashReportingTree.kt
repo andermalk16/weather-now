bee46b454d63952b1f51e73bbad53fb89fce6319
andersoncfsilva <andermalk16@gmail.com>
2017-08-20 02:39:05 -0300

    added Firebase Crash Report

--- app/src/main/java/br/com/andesoncfsilva/weathernow/utils/CrashReportingTree.kt
+++ app/src/main/java/br/com/andesoncfsilva/weathernow/utils/CrashReportingTree.kt
@@ -1,6 +1,7 @@
 package br.com.andesoncfsilva.weathernow.utils
 
 import android.util.Log
+import com.google.firebase.crash.FirebaseCrash
 import timber.log.Timber
 
 
@@ -12,10 +13,17 @@ class CrashReportingTree : Timber.Tree() {
 
     override fun log(priority: Int, tag: String, message: String, e: Throwable?) {
         try {
-            //TODO: CALL FIREBASE AND FABRIC
+
+            FirebaseCrash.log(message)
+
+            e?.let {
+                if (priority == Log.ERROR) {
+                    FirebaseCrash.report(it)
+                }
+            }
 
         } catch (e: Exception) {
-            Log.e(WNLog.TAG, "Erro Log", e)
+            Log.e("Log", "Erro Log Firebase", e)
         }
     }
 
